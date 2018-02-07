260f44be9d334d91f0b6cee1737628353aa3f3e7
Akshay <09akshay09@gmail.com>
2018-02-08 00:47:22 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -4,8 +4,8 @@
  * Stores the information about the database
  */
 // db connection parameters
-	$host = 'localhost';
-	$dbname = 'id3191589_chat1';
-	$username = 'id3191589_aks';
-	$password = 'jinjerbread';
+	$host = 
+	$dbname = 
+	$username = 
+	$password =
 ?>
